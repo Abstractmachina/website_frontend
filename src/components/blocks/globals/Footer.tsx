95e1e28bce1966cdd0ba6842058936c7164feb5a
Tao <taole.chen@gmail.com>
2024-06-11 09:42:33 +0200

    implemented globals

---
+++ src/components/blocks/globals/Footer.tsx
@@ -0,0 +1,23 @@
+import { Footer } from '@/types/payload-types'
+import Link from 'next/link'
+import React, { FC } from 'react'
+
+
+type FooterProps = {
+    content? : Footer
+}
+
+const FooterBlock: FC<FooterProps> = ({ content }) => {
+
+  return (
+      <div className='w-screen bg-pink-100 py-4 px-6 flex justify-between'>
+          {content?.bottomNavLinks?.map(li => {
+              return (
+                  <Link key={li.id} href={li.link!}>{li.label}</Link>
+              )
+          })}
+    </div>
+  )
+}
+
+export default FooterBlock;
