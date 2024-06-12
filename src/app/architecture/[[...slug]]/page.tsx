772bced108fba15389f3b01c1655e0b7300d6e98
Tao <taole.chen@gmail.com>
2024-06-12 21:16:29 +0200

    refactor

--- src/app/architecture/[[...slug]]/page.tsx
+++ src/app/architecture/[[...slug]]/page.tsx
@@ -1,53 +1,57 @@
-import RenderBlocks from '@/components/RenderBlocks'
-import { Page } from '@/types/payload-types';
-import React from 'react'
+import RenderBlocks from "@/components/RenderBlocks";
+import { Project } from "@/types/payload-types";
+import React from "react";
 
 export async function generateStaticParams() {
-  const pages = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + '/api/pages').then((res) => res.json())
-  
-  return pages.docs.map((doc:any) => ({
+  const pages = await fetch(
+    process.env.NEXT_PUBLIC_BACKEND_URL + "/api/pages"
+  ).then((res) => res.json());
+
+  return pages.docs.map((doc: any) => ({
     slug: [doc.slug],
   }));
 }
 
+async function fetchProject(slug: string): Promise<Project | undefined> {
+  const projectRequest = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/projects?where[slug][equals]=${slug}`, { next: { tags: ['projects'] } }
+  );
+  const projectData = await projectRequest.json();
 
-async function fetchProject(slug: string) : Promise<Page | undefined> {
-  const pageRequest = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/projects?where[slug][equals]=${slug}`);
-  const pageData = await pageRequest.json();
-
-
-  if (pageData.docs.length === 1) {
-    console.log("page found");
-    const page = pageData.docs[0] as Page;
+  if (projectData.docs.length === 1) {
+    console.log("project found");
+    const page = projectData.docs[0] as Project;
     return page;
-  } else if (pageData.docs.length === 0){
-    console.log("page not found");
+  } else if (projectData.docs.length === 0) {
+    console.log("project not found");
     return undefined;
   } else {
-    throw new Error('Something is not right with the query');
+    throw new Error("Something is not right with the query");
   }
 }
 
 async function ProjectPage({ params }: { params: { slug?: string[] } }) {
-
-
   // if no slugs are present, show home page
   if (!params.slug) {
-    return (<div>Architecture Home Page</div>);
+    return <div>Architecture Home Page</div>;
   }
 
-  const page = await fetchProject(params.slug[0]);
+  const project = await fetchProject(params.slug[0]);
 
-  if (!page) {
-    return (
-      <div>Page not found</div>
-    )
+  if (!project) {
+    return <div>Page not found</div>;
   }
 
   return (
-    <section className='overflow-y-auto flex flex-col h-auto'>
-      <RenderBlocks layout={page.layout} />
-    </section>
+    <article>
+      <h2>{project.title}</h2>
+      <h4>{project.subtitle}</h4>
+      <div>{project.year}</div>
+      <div>{project.location}</div>
+      <section className="overflow-y-auto flex flex-col h-auto w-full">
+        <RenderBlocks layout={project.layout} />
+      </section>
+    </article>
   );
 }
 
