ef1b2d36f0e92275b0408a2c79779bdcc1032b9b
Tao <taole.chen@gmail.com>
2024-06-12 21:16:50 +0200

    added remote pattern for bunny pull zone

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,9 @@ const nextConfig = {
             },
             {
                 protocol: 'https',
-                hostname: 'taos-pullzone.b-cdn.net'
+                hostname: 'taos-pullzone.b-cdn.net',
+                port: '',
+                pathname: '/media/**'
             }
         ]
     }
