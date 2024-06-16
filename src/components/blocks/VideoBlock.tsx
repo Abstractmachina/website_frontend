cac41e161d37919b006edc4c6157d4c97209e4a4
Tao <taole.chen@gmail.com>
2024-06-16 16:37:37 +0200

    fixed compiler error

--- src/components/blocks/VideoBlock.tsx
+++ src/components/blocks/VideoBlock.tsx
@@ -2,6 +2,7 @@ import { Project, Video } from "@/types/payload-types";
 import React, { FC } from "react";
 import ReactPlayer from "react-player";
 import VideoContainer from "../VideoContainer";
+import { ProviderData } from "@/types/video-types";
 
 type VideoBlockProps = Extract<
   NonNullable<Project["layout"]>[0],
@@ -14,37 +15,45 @@ const VideoBlock: FC<VideoBlockProps> = ({
   internalVideo,
   caption,
 }) => {
-
   function generateBunnyPath(data: Video) {
-    return `${data.providerData['libraryId']}/${data.providerData!['id']}`;
+    if (data) {
+      const provider =
+        data.providerData as unknown[] as unknown as ProviderData;
+      return `${provider.libraryId}/${provider.id}`;
+    }
+    return "";
   }
 
   if (sourceType === "external") {
     return (
       <div>
-        <VideoContainer url={externalUrl!}/>
-              </div>
-    )
-  }
-  else {
+        <VideoContainer url={externalUrl!} />
+      </div>
+    );
+  } else {
     const filedata = internalVideo as Video;
     const videourl = generateBunnyPath(filedata);
 
     return (
-      <div
-        style={{ position: 'relative', paddingTop: "56.25%" }}
-      >
-        <iframe
-          src={`https://iframe.mediadelivery.net/embed/${videourl}?autoplay=false`}
-          loading="lazy"
-          style={{border: 'none', position: 'absolute', top: 0, height: '100%', width: '100%'}} 
-          allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
-          allowFullScreen
-        >
-  </iframe>
-</div>
+      <div>
+        <div style={{ position: "relative", paddingTop: "56.25%" }}>
+          <iframe
+            src={`https://iframe.mediadelivery.net/embed/${videourl}?autoplay=false`}
+            loading="lazy"
+            style={{
+              border: "none",
+              position: "absolute",
+              top: 0,
+              height: "100%",
+              width: "100%",
+            }}
+            allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className="text-xs mt-2">Caption: {caption ? caption : filedata.caption}</div>
+      </div>
     );
-
   }
 };
 
